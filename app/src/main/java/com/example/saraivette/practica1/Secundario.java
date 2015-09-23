c3829b3e836e5e475160b6b9271d240b297f16d3
IvetteGoes <ivette.tapatia@hotmail.com>
2015-09-23 00:44:40 -0500

    validacion

---
+++ app/src/main/java/com/example/saraivette/practica1/Secundario.java
@@ -0,0 +1,20 @@
+package com.example.saraivette.practica1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by SaraIvette on 22/09/2015.
+ */
+public class Secundario extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segundolayout);
+
+    }
+}
